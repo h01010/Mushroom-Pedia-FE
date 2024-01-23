030039c3fb7db5312314f62db8a3be222c5c38bc
Yeonju Seo(서연주) <44168693+h01010@users.noreply.github.com>
2024-01-23 19:31:24 +0900

    refactor: update App style and index file style

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,16 @@ import Footer from './components/layouts/Footer';
 function App() {
   return (
     <div className="App">
-      <div style={{ border: '1px solid whitesmoke', minHeight: '80%' }}>
-        <Router>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Navigate replace to="/home" />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/list" element={<List />} />
-            <Route path="/ai-search" element={<AISearch />} />
-            <Route path="*" element={<h1>404</h1>} />
-          </Routes>
-        </Router>
-      </div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/ai-search" element={<AISearch />} />
+          <Route path="*" element={<h1>404</h1>} />
+        </Routes>
+      </Router>
       <Footer />
     </div>
   );
