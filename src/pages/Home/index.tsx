c2e8e451ebe11e4f8624390716cadb719d2e7008
Yeonju Seo(서연주) <44168693+h01010@users.noreply.github.com>
2024-01-23 19:54:04 +0900

    feat: update home page component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,64 @@
-import { HomeBox } from './style';
+import { Link } from 'react-router-dom';
+import { HomeBox, HomeTitleBox, HomeDescBox } from './style';
+import {
+  ShadowBox,
+  H3Highlight,
+  ShadowButton,
+} from '../../components/shared/shared-styled';
+import amanitamuscaria_img from '../../assets/images/amanitamuscaria_img.png';
+import leucocybeconnata_img from '../../assets/images/leucocybeconnata_img.png';
+import omphalotusolearius_img from '../../assets/images/omphalotusolearius_img.png';
 
 const Home = () => {
   return (
     <HomeBox>
-      <div>title</div>
-      <div>description of #F1</div>
-      <div>description of #F2</div>
+      <HomeTitleBox>
+        <span>Welcome To </span>
+        <br />
+        <span>Mushroom-Pedia</span>
+      </HomeTitleBox>
+      <ShadowBox className="home__description__box" id="feature-1">
+        <HomeDescBox>
+          <H3Highlight>FEATURE #1</H3Highlight>
+          <div id="home-description-feature1">
+            <span>Mushroom List & Information</span>
+            <span>from mushroom.world</span>
+          </div>
+          <Link to="/list">
+            <ShadowButton>Explore Feature #2</ShadowButton>
+          </Link>
+        </HomeDescBox>
+      </ShadowBox>
+      <ShadowBox className="home__description__box" id="feature-2">
+        <HomeDescBox>
+          <H3Highlight>FEATURE #2</H3Highlight>
+          <div id="home-description-feature2">
+            <span>Find Out Your Mushrooms</span>
+            <span>With Vision AI</span>
+          </div>
+          <Link to="/ai-search">
+            <ShadowButton>Explore Feature #2</ShadowButton>
+          </Link>
+        </HomeDescBox>
+      </ShadowBox>
+      <img
+        src={amanitamuscaria_img}
+        id="mushroom-img1"
+        alt="mushroom-img1"
+        className="home__image--mushroom"
+      />
+      <img
+        src={leucocybeconnata_img}
+        id="mushroom-img2"
+        alt="mushroom-img2"
+        className="home__image--mushroom"
+      />
+      <img
+        src={omphalotusolearius_img}
+        id="mushroom-img3"
+        alt="mushroom-img3"
+        className="home__image--mushroom"
+      />
     </HomeBox>
   );
 };
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -3,4 +3,63 @@ import styled from 'styled-components';
 export const HomeBox = styled.div`
   min-height: 80%;
   padding: 40px;
+  position: relative;
+
+  .home__description__box {
+    margin-bottom: 60px;
+    max-width: 80%;
+  }
+
+  .home__image--mushroom {
+    position: absolute;
+  }
+
+  #mushroom-img1 {
+    width: 29%;
+    top: 5%;
+    left: 0;
+    z-index: -1;
+    transform: rotate(-10deg);
+  }
+
+  #mushroom-img2 {
+    width: 26%;
+    top: 35%;
+    right: 0;
+    z-index: 70;
+    transform: rotate(10deg);
+  }
+
+  #mushroom-img3 {
+    width: 25%;
+    top: 70%;
+    left: 25%;
+    z-index: -1;
+  }
+`;
+
+export const HomeTitleBox = styled.div`
+  font-size: 35px;
+  margin-bottom: 40px;
+
+  span {
+    margin-bottom: 8px;
+    letter-spacing: 5px;
+  }
+`;
+
+export const HomeDescBox = styled.div`
+  background-color: rgb(35, 35, 35, 0.5);
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+
+  span {
+    letter-spacing: 1.5px;
+    display: block;
+    margin-bottom: 6px;
+  }
 `;
